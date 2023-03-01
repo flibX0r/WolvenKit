ad4a36a395526c5880cc994669f66e0d91ae09ac
Reece Sheppard <reece@flib.com.au>
2023-03-01 14:18:05 +0800

    New RDT View

---
+++ WolvenKit.App/ViewModels/Documents/RDTInkEditorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WolvenKit.RED4.Types;
+
+namespace WolvenKit.App.ViewModels.Documents;
+public partial class RDTInkEditorViewModel : RedDocumentTabViewModel
+{
+    public inkWidgetLibraryResource library;
+
+    public override ERedDocumentItemType DocumentItemType => ERedDocumentItemType.W2rcBuffer;
+
+    public RDTInkEditorViewModel(inkWidgetLibraryResource lib, RedDocumentViewModel file)
+        : base(file, "Ink Editor") => library = lib;
+}
